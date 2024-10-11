3ec5988340ad88c5ccc5bdb9594fc7e0a96706bc
Minotaur-ZAOU <ichiharuhidaka@gmail.com>
2024-10-11 18:28:31 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
+const form = document.querySelector("form");
+const url = document.querySelector("input");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    var videoUrl = url.value;
+    console.log(videoUrl);
+    var video = document.createElement("video");
+    video.src = "/api/view?url=" + encodeURIComponent(videoUrl);
+    video.controls = true;
+    video.playsInline = true;
+    document.body.appendChild(video);
+});
 
